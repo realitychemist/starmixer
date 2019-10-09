258fe14c60bca04e50cec352438049ec7b91c2b1
realitychemist <chas.s.evans@gmail.com>
2019-10-09 17:31:28 -0400

    Cleaned up protostar a bit, merged prototypes (a single MS class)

--- mixer/stargen.py
+++ mixer/stargen.py
@@ -2,12 +2,14 @@ import math
 import scipy
 
 # All stars have mass and age as free parameters
+# They also have metallicity as a free parameter, but that is likely out of scope
 # Stars should have an age (in MY) greater than 0 and less than 13800
 # Stars should have a mass (in MSun) between 0.01 and 395
 # Lower mass objects are not stars; the upper limit is based on max est mass for R136a1
-# Consider adding population class for stars (1, 2, or 3) based on metallicity (epoch)
 class Star:
     def __init__(self, mass, age):
+        assert self.age >= 0 and self.age <= 13800, "Invalid stellar age."
+        assert self.mass >= 0.01 and self.mass <= 395, "Invalid stellar mass."
         self.mass = mass
         self.age = age
 
@@ -18,80 +20,60 @@ class Star:
 # Protostars should not have planets, but may have accretion disks and envelopes
 # See: https://astronomy.stackexchange.com/questions/764/timescale-of-ignition-of-a-protostar
 class Protostar(Star):
+
     def __init__(self, mass, age):
         assert self.age >= 0 and self.age <= 0.5, "Incorrect age for a protostar."
         ## --==-- RADIUS --==-- ##
         # TODO: Check edge cases on math, esp. max age
-        # Critical Jeans radius assumes T=20K, particle mass=3.9e-24
         # Using the Jeans radius as initial radius at age = 0 MY
-        jeans_radius = ((scipy.G*self.mass*3.9e-24*1.989e+30) / (scipy.k*20))
-        # Final radius is equal to BD/PMS/MMS radius (continuity)
-        if self.mass < 0.08: # Brown dwarf continuity condition
-            final_radius = 0 # FIXME: Needs actual BD radius eqn
-        elif self.mass < 1: # Lower mass main sequence continuity
-            final_radius = slef.mass**0.8
-        else: # Higher mass main sequence continuity;
-            final_radius = self.mass**0.57
+        inital_radius = jeans_radius(self.mass)
+        # Final radius by main sequence continuity
+        final_radius = 0 # FIXME: Should call actual radius function for BD/PMS/MS
         # Radius is based on collapse from Jeans radius to final radius
         # Collapse happens in freefall time
         # See: http://www.astro.uu.se/~hoefner/astro/teach/apd_files/apd_collapse.pdf
-        jeans_density = self.mass / ((4/3)*math.pi*jeans_radius**3)
+        density = jeans_density(self.mass)
         freefall_time = ((3*math.pi/(32*scipy.G*jeans_density))**(1/2)/3.154e13)
         # Note: The following is just a linear interpolation, not physically accurate
-        self.radius = ((self.age * (final_radius-jeans_radius)/freefall_time) + jeans_radius)
+        self.radius = ((self.age * (final_radius-initial_radius)/freefall_time) + initial_radius)
         assert self.radius > 0, "Stellar radius less than zero!"
-
         ## --==-- TEMPERATURE --==-- ##
         initial_temp = 20
         # Continuity condition
-        if self.mass < 0.08: # Brown dwarf continuity
-            final_temp = 0 # FIXME: Needs actual BD temp eqn
-        elif self.mass < 1: # Lower mass MS continuity
-            final_temp = 0 # FIXME: Needs actual LMS temp eqn
-        else: # Higher mass MS continuity
-            final_temp = 0 # FIXME: Needs actual HMS temp eqn
-
-        # Linear interpolation over freefall time
+        final_temp = 0 # FIXME: Should call actual temp function for BD/PMS/MS
+        # Linear interpolation over freefall time (again, not realistic)
         self.temperature = ((self.age * (final_temp-initial_temp)/freefall_time) + initial_temp)
         assert self.temperature >= 20, "Protostar cooler than star forming medium!"
-
+        ## --==-- LUMINOSITY --==-- ##
+        # Function of temperature
         ## --==-- COLOR --==-- ##
         # Implement w/ colorpy
 
-        ## --==-- LUMINOSITY --==-- ##
-        # Function of temperature
+    def jeans_radius(mass):
+        # Critical Jeans radius assumes T=20K, particle mass=3.9e-24
+        return ((scipy.G*mass*3.9e-24*1.989e+30) / (scipy.k*20))
 
+    def jeans_density(mass):
+        return mass / ((4/3)*math.pi*jeans_radius(mass)**3)
 
-# A pre-main-sequence star is older than a protostar, but still young (<20 MY)
+# A pre-main-sequence star is older than a protostar, but still young (<~20 MY)
 # These stars should have mass between 0.08 and 10 MSun
+# More massive stars emerge directly onto the main sequence
 class Pre_Main_Sequence(Star):
     pass
+
+
+# The main section of stellar life
+# See: http://personal.psu.edu/rbc3/A534/lec18.pdf
+# See: http://hyperphysics.phy-astr.gsu.edu/hbase/Astro/startime.html
+class Main_Sequence(Star):
+    pass
+
 
 # Brown dwarves form from protostars which accreted less than 0.08 MSun
 # There is no upper limit on the age of a brown dwarf, but they cool steadily
 # See: http://www2.ifa.hawaii.edu/CSPF/presentations/bdtutorial/bdtutorial.pdf
 class BrownDwarf(Star):
-    pass
-
-
-# Masses between 0.08 MSun and 0.6 MSun
-# No upper limit on age, should be at least 20 MY old
-# See: http://personal.psu.edu/rbc3/A534/lec18.pdf
-class Low_Mass_Main_Sequence(Star):
-    pass
-
-
-# Masses between 0.6 and 10 MSun
-# Age between 20 MY and 10000 MY (lifetime depends on mass)
-# See: http://hyperphysics.phy-astr.gsu.edu/hbase/Astro/startime.html
-class Midsize_Main_Sequence(Star):
-    pass
-
-
-# Masses above 10 MSun
-# Age between 0.1 MY and 32 MY (lifetime depends on mass)
-# See: http://hyperphysics.phy-astr.gsu.edu/hbase/Astro/startime.html
-class Massive_Main_Sequence(Star):
     pass
 
 
