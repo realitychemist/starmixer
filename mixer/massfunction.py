a4641d1fca2a9121025951effbe7dc169cbef760
realitychemist <chas.s.evans@gmail.com>
2020-02-14 16:44:31 -0500

    Restructured stargen (new paper), adjusted L3 massfunction defaults

--- mixer/massfunction.py
+++ mixer/massfunction.py
@@ -2,22 +2,22 @@ import scipy.integrate as integrate
 import math
 
 class L3:
-    # Default L3 IMF values are defined in https://arxiv.org/pdf/1212.0939.pdf, except for the normalization factor.
-    # The normalization factor was derived by integrating the L3 IMF over the range [0,1] and setting this equal to 1.
+    # Default L3 IMF values are defined in https://arxiv.org/pdf/1212.0939.pdf; all constants are for single stars
+    # The default normalization factor is correct for the mass range [0.01,150] (solar mass units)
     def __init__(self, alpha = 2.3, beta = 1.4,
-                 mu = 0.2, normal = 2.723881205):
+                 mu = 0.2, normal = 3.2869181018430393):
         self.alpha = alpha
         self.beta = beta
         self.mu = mu
         self.normal = normal
 
-    def pdf(self, x):
+    def pdf(self, mass):
         return (self.normal
-                * (x/self.mu)**(-1*self.alpha)
-                * (1+(x/self.mu)**(1-self.alpha))**(-1*self.beta))
+                * (mass/self.mu)**(-1*self.alpha)
+                * (1+(mass/self.mu)**(1-self.alpha))**(-1*self.beta))
 
     # This method is used to set self.normal automatically, in case parameters or bounds of integration are changed.
-    def renomalize(self, min, max):
+    def renormalize(self, min, max):
         total_prob = integrate.quad(lambda x: self.normal
                         * (x/self.mu)**(-1*self.alpha)
                         * (1+(x/self.mu)**(1-self.alpha))**(-1*self.beta),
--- mixer/stargen.py
+++ mixer/stargen.py
@@ -1,82 +1,42 @@
 import math
 import scipy
 
-# All stars have mass and age as free parameters
-# They also have metallicity as a free parameter, but that is likely out of scope
-# Stars should have an age (in MY) greater than 0 and less than 13800
-# Stars should have a mass (in MSun) between 0.01 and 395
-# Lower mass objects are not stars; the upper limit is based on max est mass for R136a1
+# All stars have three free parameters:
+#       * Mass (in Msun, solar mass units)
+#       * Age (in Myr, millions of years)
+#       * Metallicity (in Z, mass fraction of elements other than H or He)
+# Stars should have an age (in Myr) less than 13800 *unless* they are meant to be
+#  hypothetical future stars.  An age of 0 represents emergence onto the main
+#  sequence, while negative ages represent PMS stars, protostars, and unstable dust clouds.
+# Stars should have a mass (in MSun) between 0.01 and 150.  Other ranges require
+#  renormalization of the L3 massfunction.
+# Stars must, by definition, have a metallicity in the range [0,1]
+#
+# This new approach is due to the paper "Comprehensive analytic formulae for stellar
+#  evolution as a function of mass and metallicity" by Hurley, Pols, and Tout (2000).
+#  See https://arxiv.org/abs/astro-ph/0001295
+
 class Star:
-    def __init__(self, mass, age):
-        assert self.age >= 0 and self.age <= 13800, "Invalid stellar age."
-        assert self.mass >= 0.01 and self.mass <= 395, "Invalid stellar mass."
+    def __init__(self, mass, age, metallicity):
+        assert self.age >= 0, "Pre-ZAMS object, use PreStar class"
+        assert self.mass >= 0.01 and self.mass <= 150, "Stellar mass outside callibrated range!"
+        assert self.metallicity >= 0 and self.metallicity <= 1, "Nonsensical metallicity!"
         self.mass = mass
         self.age = age
+        self.metallicity = metallicity
 
+    # Set the time-invariant constants needed to follow the star through its evolution
+    def setConstants(self):
+        # Logarithmic metallicity relative to Sol, analogous to [Fe/H]
+        log_metallicity = math.log((self.metallicity / 0.02), 10)
+        # Mass above which a main sequence hook appears
+        mass_hook = 1.0185 + 0.16015*log_metallicity + 0.0892*log_metallicity**2
+        # Maximum mass for which a helium flash occurs
+        mass_HeF = 1.995 + 0.25*log_metallicity + 0.087*log_metallicity**2
+        # Maximum mass for which helium ignites on the first giant branch
+        mass_FGB = (13.048*(log_metallicity/0.02)**0.06)/(1 + 0.0012*(0.02/log_metallicity)**1.27)
+        
 
-# Protostars are young stars of any size
-# Protostar objects should be created for stars between 0 and 0.5 MY old
-# See: https://arxiv.org/pdf/1401.1809.pdf
-# Protostars should not have planets, but may have accretion disks and envelopes
-# See: https://astronomy.stackexchange.com/questions/764/timescale-of-ignition-of-a-protostar
-class Protostar(Star):
-
-    def __init__(self, mass, age):
-        assert self.age >= 0 and self.age <= 0.5, "Incorrect age for a protostar."
-        ## --==-- RADIUS --==-- ##
-        # TODO: Check edge cases on math, esp. max age
-        # Using the Jeans radius as initial radius at age = 0 MY
-        inital_radius = jeans_radius(self.mass)
-        # Final radius by main sequence continuity
-        final_radius = 0 # FIXME: Should call actual radius function for BD/PMS/MS
-        # Radius is based on collapse from Jeans radius to final radius
-        # Collapse happens in freefall time
-        # See: http://www.astro.uu.se/~hoefner/astro/teach/apd_files/apd_collapse.pdf
-        density = jeans_density(self.mass)
-        freefall_time = ((3*math.pi/(32*scipy.G*jeans_density))**(1/2)/3.154e13)
-        # Note: The following is just a linear interpolation, not physically accurate
-        self.radius = ((self.age * (final_radius-initial_radius)/freefall_time) + initial_radius)
-        assert self.radius > 0, "Stellar radius less than zero!"
-        ## --==-- TEMPERATURE --==-- ##
-        initial_temp = 20
-        # Continuity condition
-        final_temp = 0 # FIXME: Should call actual temp function for BD/PMS/MS
-        # Linear interpolation over freefall time (again, not realistic)
-        self.temperature = ((self.age * (final_temp-initial_temp)/freefall_time) + initial_temp)
-        assert self.temperature >= 20, "Protostar cooler than star forming medium!"
-        ## --==-- LUMINOSITY --==-- ##
-        # Function of temperature
-        ## --==-- COLOR --==-- ##
-        # Implement w/ colorpy
-
-    def jeans_radius(mass):
-        # Critical Jeans radius assumes T=20K, particle mass=3.9e-24
-        return ((scipy.G*mass*3.9e-24*1.989e+30) / (scipy.k*20))
-
-    def jeans_density(mass):
-        return mass / ((4/3)*math.pi*jeans_radius(mass)**3)
-
-# A pre-main-sequence star is older than a protostar, but still young (<~20 MY)
-# These stars should have mass between 0.08 and 10 MSun
-# More massive stars emerge directly onto the main sequence
-class Pre_Main_Sequence(Star):
+# A class for stars with an age less than 0 (pre-ZAMS)
+class PreStar(Star):
     pass
-
-
-# The main section of stellar life
-# See: http://personal.psu.edu/rbc3/A534/lec18.pdf
-# See: http://hyperphysics.phy-astr.gsu.edu/hbase/Astro/startime.html
-class Main_Sequence(Star):
-    pass
-
-
-# Brown dwarves form from protostars which accreted less than 0.08 MSun
-# There is no upper limit on the age of a brown dwarf, but they cool steadily
-# See: http://www2.ifa.hawaii.edu/CSPF/presentations/bdtutorial/bdtutorial.pdf
-class BrownDwarf(Star):
-    pass
-
-
-# TODO: Prototype post-main-sequence stars and stellar remnants
-# PMS star types: AG, RG, RSG, BSG, LBV, YHG, WR
-# Remnant types: White_Dwarf, Neutron_Star (multiple types), Black_Hole
